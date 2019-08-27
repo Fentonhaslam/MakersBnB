1e88cc4b8256c22dd2a3cf4f8b4fdbd32bd98812
fentonhaslam <fentonhaslam@gmail.com>
2019-08-27 15:21:29 +0100

    created create method with passing tests

--- lib/space.rb
+++ lib/space.rb
@@ -1,13 +1,27 @@
 
 class Space
+  attr_reader :id, :description, :title, :price
+
+  def initialize(id:, title:, price:, description:)
+    @id = id
+    @title = title
+    @price = price
+    @description = description
+  end
+
   def self.all
-    result = DatabaseConnection.query('SELECT * FROM spaces')
+    result = DatabaseConnection.query("SELECT * FROM spaces")
     spaces = []
     result.each do |space|
-      spaces.push(space['title'])
-      spaces.push(space['price'])
-      spaces.push(space['description'])
+      spaces.push(space["title"])
+      spaces.push(space["price"])
+      spaces.push(space["description"])
     end
     return spaces
+  end
+
+  def self.create(title:, price:, description:)
+    result = DatabaseConnection.query("INSERT INTO spaces (title, price, description) VALUES('#{title}', '#{price}', '#{description}') RETURNING id, title, price, description;")
+    Space.new(id: result[0]["id"], title: result[0]["title"], price: result[0]["price"], description: result[0]["description"])
   end
 end
