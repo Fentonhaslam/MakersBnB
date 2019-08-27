f5f111b374ed6f17c76eb11e4ab27d6dca5fa29d
RussellWind <russell.wind@gmail.com>
2019-08-27 12:35:38 +0100

    Set up database testing environment

--- db/migrations/02_create_spaces_table.sql
+++ db/migrations/02_create_spaces_table.sql
@@ -1,1 +1,1 @@
-CREATE TABLE spaces(id SERIAL PRIMARY KEY, title VARCHAR(60), price INTEGER(8), description VARCHAR(240));
+CREATE TABLE spaces(id SERIAL PRIMARY KEY, title VARCHAR(60), price INT, description VARCHAR(240));
